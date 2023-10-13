6bd4cf24e4de299710d3c0672a1cb716a7e9e979
Atish Jadhav <atishjadhav835@gmail.com>
2023-10-13 15:52:49 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,42 @@
-import React from "react";
 import "./App.css";
-import Tictactoe from "./components/Tictactoe";
 
-const App = () => {
+import React, { useState } from "react";
+
+let adjustInterval = undefined;
+
+function App() {
+  const [watch, setWatch] = useState(0);
+  const [started, setStarted] = useState(false);
+
+  const startWatch = () => {
+    adjustInterval = setInterval(() => {
+      setWatch((x) => x + 1);
+    }, 1000);
+    setStarted(true);
+  };
+  const stopWatch = () => {
+    clearInterval(adjustInterval);
+    setStarted(false);
+  };
+  const resetWatch = () => {
+    setWatch(0);
+  };
+
   return (
-    <div className="app">
-      <Tictactoe/>
+    <div className="App">
+      <h1>Start Stop watch</h1>
+      <h1>{watch}</h1>
+      <button disabled={started} className="start" onClick={startWatch}>
+        Start
+      </button>
+      <button className="stop" onClick={stopWatch}>Stop</button>
+      <button className="reset" onClick={resetWatch}>Reset</button>
     </div>
   );
-};
+}
 
 export default App;
+
+
 
 
